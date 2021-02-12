593e7bd5fb04db188e388e126e25bb26636512c6
unknown <peters0470@gmail.com>
2021-02-12 17:03:15 -0600

    removed comment

--- models/Comment.js
+++ models/Comment.js
@@ -45,7 +45,7 @@ const CommentSchema = new Schema(
       default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
     },
-    // use ReplySchema to validate data for a reply
+    
     replies: [ReplySchema]
   },
   {
